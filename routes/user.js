0bda11a94686237f9750fc9f23d29977471e81b8
opheliechoisi <ochoisi@yahoo.com>
2025-05-19 15:57:33 +0200

    modif in sha256 in lower case

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const uid2 = require("uid2");
-const SHA256 = require("crypto-js/SHA256");
+const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
 
 const User = require("../models/User");
